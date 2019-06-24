be853bbbaf66cbc7f5d2c62f06475b3f6575fd65
Maximilian Wallinger <maximilian.wallinger@outlook.com>
2019-06-24 13:33:35 +0200

    additional shaders

---
+++ simple_color2_fs.glsl
@@ -0,0 +1,9 @@
+#version 330 core
+
+in vec4 passColor;
+out vec4 fragmentColor;
+
+void main()
+{
+    fragmentColor = passColor;
+}
---
+++ simple_color2_vs.glsl
@@ -0,0 +1,11 @@
+#version 330 core
+
+in vec3 aPos;
+out vec4 passColor;
+uniform mat4 modlMat, viewMat, projMat;
+
+void main()
+{
+    gl_Position = projMat * viewMat * modlMat * vec4(aPos, 1.0);
+    passColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
